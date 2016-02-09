0ad649ae84f86014ae02de891f4702dd6438be94
utercero <utercero@gmail.com>
2016-02-09 14:08:09 +0100

    Nuevo proyecto

---
+++ NewForm/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NewForm
+{
+    public static class Program
+    {
+        public static Dictionary<string,XmlNode> 
+        /// <summary>
+        /// Punto de entrada principal para la aplicación.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            //Application.Run(new Form1());
+        }
+    }
+}
