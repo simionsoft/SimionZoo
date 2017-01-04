a8746e54fb17cd0eb2f1915d0d1550fbe36ad2ab
borjafdezgauna <borja.fernandez@ehu.eus>
2017-01-04 16:00:44 +0100

    changes in min/max values of world-FAST

--- RLSimion-Lib/world-FAST.cpp
+++ RLSimion-Lib/world-FAST.cpp
@@ -21,25 +21,6 @@ CFASTWindTurbine::CFASTWindTurbine(CConfigNode* pConfigNode)
 {
 	METADATA("World", "FAST-Wind-turbine");
 
-	addStateVariable("T_a", "N/m", 0.0, 400000.0);
-	addStateVariable("P_a", "W", 0.0, 1600000.0);
-	addStateVariable("P_s", "W", 500000.0, 700000.0);
-	addStateVariable("P_e", "W", 500000.0, 700000.0);
-	addStateVariable("E_p", "W", -100000, 100000);
-	addStateVariable("v", "m/s", 1.0, 50.0);
-	addStateVariable("omega_r", "rad/s", 2.39823, 6.39823);
-	addStateVariable("omega_g", "rad/s", 0.0, 200.0);
-	addStateVariable("E_omega_r", "rad/s", -4.0, 4.0);
-	addStateVariable("d_omega_r", "rad/s^2", -2.0, 2.0);
-	addStateVariable("beta", "rad", 0.0, 1.570796);
-	addStateVariable("d_beta", "rad/s", -0.1396263, 0.1396263);
-	addStateVariable("T_g", "N/m", 0.0, 47402.91);
-	addStateVariable("d_T_g", "N/m/s", -15000, 15000);
-	addStateVariable("E_int_omega_r", "rad", -100.0, 100.0);
-	//action handlers
-	addActionVariable("d_beta", "rad/s", -0.1396263, 0.1396263);
-	addActionVariable("d_T_g", "N/m/s", -15000.0, 15000.0);
-
 	//model constants
 	addConstant("RatedPower", 5e6);				//W
 	addConstant("HubHeight", 90);				//m
@@ -57,6 +38,27 @@ CFASTWindTurbine::CFASTWindTurbine(CConfigNode* pConfigNode)
 	addConstant("GeneratorInertia", 534116.0);			//kg*m^2
 	addConstant("HubInertia", 115926.0);				//kg*m^2
 	addConstant("DriveTrainTorsionalDamping", 6210000.0); //N*m/(rad/s)
+
+	addStateVariable("T_a", "N/m", 0.0, 400000.0);
+	addStateVariable("P_a", "W", 0.0, 1600000.0);
+	addStateVariable("P_s", "W", 0.0, 6e6);
+	addStateVariable("P_e", "W", 0.0, 6e6);
+	addStateVariable("E_p", "W", -5e6,5e6);
+	addStateVariable("v", "m/s", 1.0, 50.0);
+	addStateVariable("omega_r", "rad/s", 0.0, 6.0);
+	addStateVariable("omega_g", "rad/s", 0.0, 200.0);
+	addStateVariable("E_omega_r", "rad/s", -4.0, 4.0);
+	addStateVariable("d_omega_r", "rad/s^2", -2.0, 2.0);
+	addStateVariable("beta", "rad", 0.0, 1.570796);
+	addStateVariable("d_beta", "rad/s", -0.1396263, 0.1396263);
+	addStateVariable("T_g", "N/m", 0.0, 50000.0);
+	addStateVariable("d_T_g", "N/m/s", -15000, 15000);
+	addStateVariable("E_int_omega_r", "rad", -100.0, 100.0);
+	//action handlers
+	addActionVariable("d_beta", "rad/s", -0.1396263, 0.1396263);
+	addActionVariable("d_T_g", "N/m/s", -15000.0, 15000.0);
+
+
 
 	m_pRewardFunction->addRewardComponent(new CToleranceRegionReward("E_p", 100, 1.0));
 	m_pRewardFunction->initialize();
