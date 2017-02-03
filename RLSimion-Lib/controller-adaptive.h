9b34e3b25fc0415b75b5f4e9c096c146908fc6e6
borjafdezgauna <borja.fernandez@ehu.eus>
2017-02-03 20:11:58 +0100

    Debug version of FASTDimensionalPortal outputs again to /debug directory. If debug versions are run remotely on machines without VStudio, it won't load.

--- RLSimion-Lib/controller-adaptive.h
+++ RLSimion-Lib/controller-adaptive.h
@@ -19,3 +19,33 @@ public:
 	virtual void updateValue(const CState *s, const CAction *a, const CState *s_p, double r);
 	virtual void updatePolicy(const CState *s, const CAction *a, const CState *s_p, double r);
 };
+
+class CExtendedWindTurbineBoukhezzarController : public CWindTurbineBoukhezzarController
+{
+	double m_td;
+	CHILD_OBJECT_FACTORY<CCritic> m_pCritic;
+	CHILD_OBJECT_FACTORY<CPolicyLearner> m_pAdpParamA, m_pAdpParamK_alpha, m_pAdpParamKP;
+
+public:
+	CExtendedWindTurbineBoukhezzarController(CConfigNode* pConfigNode);
+	virtual ~CExtendedWindTurbineBoukhezzarController();
+
+	void selectAction(const CState *s, CAction *a);
+	virtual void updateValue(const CState *s, const CAction *a, const CState *s_p, double r);
+	virtual void updatePolicy(const CState *s, const CAction *a, const CState *s_p, double r);
+};
+
+class CExtendedWindTurbineJonkmanController : public CWindTurbineJonkmanController
+{
+	double m_td;
+	CHILD_OBJECT_FACTORY<CCritic> m_pCritic;
+	CHILD_OBJECT_FACTORY<CPolicyLearner> m_pAdpParamA, m_pAdpParamK_alpha, m_pAdpParamKP;
+
+public:
+	CExtendedWindTurbineJonkmanController(CConfigNode* pConfigNode);
+	virtual ~CExtendedWindTurbineJonkmanController();
+
+	void selectAction(const CState *s, CAction *a);
+	virtual void updateValue(const CState *s, const CAction *a, const CState *s_p, double r);
+	virtual void updatePolicy(const CState *s, const CAction *a, const CState *s_p, double r);
+};
