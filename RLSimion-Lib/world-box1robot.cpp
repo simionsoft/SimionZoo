bacfc5c5ccb95f68e465a71cd0df450cd4d4fa5b
aserrin55 <aserre6@gmail.com>
2017-03-25 00:37:19 +0100

    The experiment looks"good" but the results are not the expected ones. Working on it.

--- RLSimion-Lib/world-box1robot.cpp
+++ RLSimion-Lib/world-box1robot.cpp
@@ -11,8 +11,15 @@ double static getDistanceBetweenPoints(double x1, double y1, double x2, double y
 	return distance;
 }
 
-#define TargetX 32.4
-#define TargetY 17.0
+#define TargetX 12.4
+#define TargetY 0.0
+
+#define robotOrigin_x 3.0
+#define robotOrigin_y 0.0
+
+#define boxOrigin_x 5.0
+#define boxOrigin_y 0.0
+
 
 CMoveBoxOneRobot::CMoveBoxOneRobot(CConfigNode* pConfigNode)
 {
@@ -23,8 +30,8 @@ CMoveBoxOneRobot::CMoveBoxOneRobot(CConfigNode* pConfigNode)
 	box_X = addStateVariable("bx", "m", -50.0, 50.0);
 	box_Y = addStateVariable("by", "m", -50.0, 50.0);
 
-	rob1_forceX = addActionVariable("r1forceX", "N", -8.0, 8.0);
-	rob1_forceY = addActionVariable("r1forceY", "N", -8.0, 8.0);
+	rob1_forceX = addActionVariable("r1forceX", "N", -20.0, 20.0);
+	rob1_forceY = addActionVariable("r1forceY", "N", -20.0, 20.0);
 
 	GRAVITY = -9.8;
 	MASS_ROBOT = 1.f;
@@ -58,7 +65,7 @@ CMoveBoxOneRobot::CMoveBoxOneRobot(CConfigNode* pConfigNode)
 		m_collisionShapes.push_back(colShape);
 		btTransform startTransform;
 		startTransform.setIdentity();
-		startTransform.setOrigin(btVector3(3.0, 0.0, 0.0));
+		startTransform.setOrigin(btVector3(boxOrigin_x, 0.0, boxOrigin_y));
 		// if mass != 0.f object is dynamic
 		btScalar	mass(MASS_BOX);
 		btVector3 localInertia(0, 0, 0);
@@ -72,11 +79,11 @@ CMoveBoxOneRobot::CMoveBoxOneRobot(CConfigNode* pConfigNode)
 
 	///creating a dynamic robot obj2 
 	{
-		btCollisionShape* robot1Shape = new btSphereShape(btScalar(1));
+		btCollisionShape* robot1Shape = new btSphereShape(btScalar(0.5));
 		m_collisionShapes.push_back(robot1Shape);
 		btTransform robot1startTransform;
 		robot1startTransform.setIdentity();
-		robot1startTransform.setOrigin(btVector3(0.5, 0.0, 0.0));
+		robot1startTransform.setOrigin(btVector3(robotOrigin_x, 0.0, robotOrigin_y));
 		// if mass != 0.f object is dynamic
 		btScalar robot1mass(MASS_ROBOT);
 		btVector3 robot1localInertia(0, 0, 0);
@@ -94,27 +101,33 @@ CMoveBoxOneRobot::CMoveBoxOneRobot(CConfigNode* pConfigNode)
 
 void CMoveBoxOneRobot::reset(CState *s)
 {
+	btTransform robotTransform;
+	btTransform boxTransform;
 
-	if (CSimionApp::get()->pExperiment->isEvaluationEpisode())
+//	if (CSimionApp::get()->pExperiment->isEvaluationEpisode())
 	{
 		//fixed setting in evaluation episodes
-		s->set(rob1_X, 0.5);
-		s->set(rob1_Y, 0.0);
-		s->set(box_X, 3.0);
-		s->set(box_Y, 0.0);
-		//faltan theta
-
+		m_pRobot1->getBody()->getMotionState()->getWorldTransform(robotTransform);
+		m_box->getBody()->getMotionState()->getWorldTransform(boxTransform);
+		robotTransform.setOrigin(btVector3(robotOrigin_x, 0.0, robotOrigin_y));
+		m_pRobot1->getBody()->setWorldTransform(robotTransform);
+		boxTransform.setOrigin(btVector3(boxOrigin_x, 0.0, boxOrigin_y));
+		m_box->getBody()->setWorldTransform(robotTransform);
+		s->set(rob1_X, robotOrigin_x);
+		s->set(rob1_Y, robotOrigin_y);
+		s->set(box_X, boxOrigin_x);
+		s->set(box_Y, boxOrigin_y);
 	}
-	else
-	{
-		//random setting in training episodes
-
-		s->set(rob1_X, 0.2 + getRandomValue()*0.4);
-		s->set(rob1_Y, 0.7 + getRandomValue()*0.2);
-		s->set(box_X, 2.5 + getRandomValue()*0.4);
-		s->set(box_Y, 0.0 + getRandomValue()*0.2);
-		//faltan theta
-	}
+	//else
+	//{
+	//	//random setting in training episodes
+	//	robotTransform.setOrigin(btVector3(robotOrigin_x + getRandomValue()*0.4, 0.0, robotOrigin_y + getRandomValue()*0.4));
+	//	boxTransform.setOrigin(btVector3(boxOrigin_x + getRandomValue()*0.4, 0.0, boxOrigin_y + getRandomValue()*0.2));
+	//	s->set(rob1_X, robotOrigin_x + getRandomValue()*0.4);
+	//	s->set(rob1_Y, robotOrigin_y + getRandomValue()*0.2);
+	//	s->set(box_X, boxOrigin_x + getRandomValue()*0.4);
+	//	s->set(box_Y, boxOrigin_y + getRandomValue()*0.2);
+	//}
 }
 
 void CMoveBoxOneRobot::executeAction(CState *s, const CAction *a, double dt)
@@ -125,13 +138,13 @@ void CMoveBoxOneRobot::executeAction(CState *s, const CAction *a, double dt)
 
 	btVector3 applied_force1;
 
-	m_dynamicsWorld->stepSimulation(dt);
 
 	btTransform box_trans;
 	btTransform r1_trans;
 
 	//Update Box
 	m_box->getBody()->getMotionState()->getWorldTransform(box_trans);
+
 	s->set(box_X, float(box_trans.getOrigin().getX()));
 	s->set(box_Y, float(box_trans.getOrigin().getZ()));
 
@@ -139,14 +152,22 @@ void CMoveBoxOneRobot::executeAction(CState *s, const CAction *a, double dt)
 	m_pRobot1->setAppliedForce(btVector3(rob1forcex, 0, rob1forcey));
 	m_pRobot1->getAppliedForce(applied_force1);
 	m_pRobot1->getBody()->applyCentralForce(applied_force1);
+
+	m_dynamicsWorld->stepSimulation(dt);
+
+	int numCollision = m_dynamicsWorld->getDispatcher()->getNumManifolds();
+
 	m_pRobot1->getBody()->getMotionState()->getWorldTransform(r1_trans);
 	s->set(rob1_X, double(r1_trans.getOrigin().getX()));
 	s->set(rob1_Y, double(r1_trans.getOrigin().getZ()));
+	double rob1x = s->get(rob1_X);
+	double rob1y = s->get(rob1_Y);
 
 }
 
 double CMoveBoxOneRobotReward::getReward(const CState* s, const CAction* a, const CState* s_p)
 {
+	CExperiment* pExperiment = CSimionApp::get()->pExperiment.ptr();
 	bool bEval = CSimionApp::get()->pExperiment->isEvaluationEpisode();
 	int step = CSimionApp::get()->pExperiment->getStep();
 
@@ -154,14 +175,13 @@ double CMoveBoxOneRobotReward::getReward(const CState* s, const CAction* a, cons
 	double boxAfterY = s_p->get("by");
 
 	double distance = getDistanceBetweenPoints(TargetX, TargetY, boxAfterX, boxAfterY);
-	if (distance < 2) {
-		return 0.9;
-	}
-	if (distance < 0.5) {
+
+	if (distance < 0.5)
+	{
 		CSimionApp::get()->pExperiment->setTerminalState();
 		return 1;
 	}
-	return 2 / distance;
+	return 1 /( distance*distance);
 }
 
 double CMoveBoxOneRobotReward::getMin()
