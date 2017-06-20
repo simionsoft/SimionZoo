e82b42720505d99f81c04c72184759c3c173f952
borjafdezgauna <borja.fernandez@ehu.eus>
2017-06-20 17:46:16 +0200

    RLSimion: -unit-test added with 2 outputs -test experiments in /examples/tests renamed

--- tests/RLSimion/StateActionVFA/unittest1.cpp
+++ tests/RLSimion/StateActionVFA/unittest1.cpp
@@ -13,7 +13,7 @@ namespace StateActionVFA
 	{
 	public:
 		
-		TEST_METHOD(StateActionFunc_QArgMax)
+		TEST_METHOD(QFunction_ArgMax)
 		{
 			//Because we are mostly only implementing the constructor that takes a CConfigNode,
 			//it is easier to load a config file and then manipulate the classes loaded
@@ -63,7 +63,7 @@ namespace StateActionVFA
 			delete pApp;
 			delete pVFA;
 		}
-		TEST_METHOD(StateActionFunc_FeatureMap)
+		TEST_METHOD(QFunction_FeatureMap)
 		{
 			//Because we are mostly only implementing the constructor that takes a CConfigNode,
 			//it is easier to load a config file and then manipulate the classes loaded
@@ -105,6 +105,68 @@ namespace StateActionVFA
 			if (abs(state - 2.5) > 0.1) Assert::Fail();
 			if (abs(action - 8) > 0.1) Assert::Fail();
 
+
+			delete pState;
+			delete pAction;
+
+			delete pFeatures;
+			delete pApp;
+			delete pVFA;
+		}
+
+		TEST_METHOD(QFunction_ArgMax_two_actions)
+		{
+			//Because we are mostly only implementing the constructor that takes a CConfigNode,
+			//it is easier to load a config file and then manipulate the classes loaded
+			CConfigFile configFile;
+			CConfigNode* pConfigNode = configFile.loadFile("..\\tests\\robot-control-test.simion.exp");
+			RLSimionApp *pApp = new RLSimionApp(pConfigNode);
+
+			CLinearStateActionVFA *pVFA = new CLinearStateActionVFA(
+				pApp->pSimGod->getGlobalStateFeatureMap()
+				, pApp->pSimGod->getGlobalActionFeatureMap());
+
+			pVFA->setInitValue(0.0);
+			pApp->pSimGod->deferredLoad();
+
+			CFeatureList *pFeatures = new CFeatureList("features");
+
+			CState* pState = pApp->pWorld->getDynamicModel()->getStateInstance();
+			CAction* pAction = pApp->pWorld->getDynamicModel()->getActionInstance();
+			//Q(s={0.0,1.0,0.0},a={1.0,1.0})= 100;
+			pState->set("rx1", 0.0);
+			pState->set("ry1", 1.0);
+			pState->set("theta", 0.0);
+			pAction->set("v", 1.0);
+			pAction->set("omega", 0.5);
+
+			pVFA->getFeatures(pState, pAction, pFeatures);
+			pFeatures->normalize();
+			pFeatures->mult(100);
+			pVFA->add(pFeatures);
+
+			for (unsigned int i = 0; i < pFeatures->m_numFeatures; i++)
+				pFeatures->m_pFeatures[i].m_factor = 1.0;
+			double value = pVFA->get(pFeatures);
+
+			//we should get the same value: 100
+			if (abs(value - 100) > 0.001) Assert::Fail();
+
+			//Q(s={0.0,1.0,0.0},a={-1.0,0.0})= 10;
+			pState->set("rx1", 0.0);
+			pState->set("ry1", 1.0);
+			pState->set("theta", 0.0);
+			pAction->set("v", -1.0);
+			pAction->set("omega", -0.5);
+
+			pVFA->getFeatures(pState, pAction, pFeatures);
+			pFeatures->mult(10);
+			pVFA->argMax(pState, pAction);
+			double argMaxV = pAction->get("v");
+			if (abs(argMaxV - 1.0) > 0.1) Assert::Fail();
+			
+			double argMaxOmega = pAction->get("omega");
+			if (abs(argMaxOmega - 0.5) > 0.1) Assert::Fail();
 
 			delete pState;
 			delete pAction;
