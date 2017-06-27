ccd48572a906903ba3819b335af9e91608a80829
borjafdezgauna <borja.fernandez@ehu.eus>
2017-06-27 17:46:09 +0200

    link and compilation small fixes

--- tools/WindowsUtils/FileUtils.cpp
+++ tools/WindowsUtils/FileUtils.cpp
@@ -2,7 +2,7 @@
 
 string getDirectory(const string& pFilename)
 {
-	int last = pFilename.find_last_of('/', '\\');
+	size_t last = pFilename.find_last_of('/', '\\');
 	if (last < 0)
 		return string("");
 	return string(pFilename.substr(0, last + 1));
