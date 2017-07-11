b43c69194c28e6a69725fc5cb2e454a1c865742e
borjafdezgauna <borja.fernandez@ehu.eus>
2017-07-11 18:41:00 +0200

    Merge commit

--- Badger/ViewModels/Reports/ReportViewModel.cs
+++ Badger/ViewModels/Reports/ReportViewModel.cs
@@ -7,7 +7,7 @@ namespace Badger.ViewModels
 {
     public class ReportViewModel : Screen
     {
-        private string m_name = "Unnamed";
+        private string m_name= "Unnamed";
         public string name { get { return m_name; } set { m_name = value; NotifyOfPropertyChange(() => name); } }
 
         //plots
@@ -21,7 +21,7 @@ namespace Badger.ViewModels
         public PlotViewModel selectedPlot
         {
             get { return m_selectedPlot; }
-            set { m_selectedPlot = value; m_selectedPlot.updateView(); NotifyOfPropertyChange(() => selectedPlot); }
+            set { m_selectedPlot = value;m_selectedPlot.updateView();  NotifyOfPropertyChange(() => selectedPlot); }
         }
 
         public void updateView() { }
@@ -31,7 +31,7 @@ namespace Badger.ViewModels
         public BindableCollection<StatsViewModel> stats
         {
             get { return m_stats; }
-            set { m_stats = value; NotifyOfPropertyChange(() => stats); }
+            set { m_stats = value;  NotifyOfPropertyChange(() => stats); }
         }
         private StatsViewModel m_selectedStat;
         public StatsViewModel selectedStat
@@ -67,7 +67,7 @@ namespace Badger.ViewModels
 
         private ReportsWindowViewModel m_parent { get; set; }
 
-        private string getVariablePlotType(LogQuery query, string variable)
+        private string getVariablePlotType(LogQuery query,string variable)
         {
             foreach (LoggedVariableViewModel var in query.loggedVariables)
             {
@@ -80,93 +80,80 @@ namespace Badger.ViewModels
         public ReportViewModel(LogQuery query, ReportsWindowViewModel parent)
         {
             m_parent = parent;
-            
-            loadAllStats(query);
-            if (stats.Count > 0)
-                selectedStat = stats[0];
-
-            loadAllPlots(query);
-            if (plots.Count > 0)
-            {
-                selectedPlot = plots[0];
-                selectedPlot.updateView();
-            }
-        }
-
-        private void loadAllStats(LogQuery query)
-        {
-            StatsViewModel newStatsViewModel;
+            //stats
+            StatsViewModel newStat;
             foreach (string variable in query.variables)
             {
-                newStatsViewModel = new StatsViewModel(variable);
-                int trackId = 0;
-                foreach (LogQueryResultTrackViewModel track in query.resultTracks)
+                newStat = new StatsViewModel(variable, this);
+                foreach(LogQueryResultTrackViewModel track in query.resultTracks)
                 {
                     TrackVariableData trackData = track.trackData.getVariableData(variable);
                     if (trackData != null)
                     {
-                        StatViewModel trackStats = new StatViewModel(track.groupId, trackId.ToString());
+                        //code below can be improved: organize forks hierarchically instead of a single string
+                        StatViewModel trackStats = new StatViewModel(track.groupId,track.trackId);
                         trackStats.lastEpisodeStats = trackData.lastEpisodeData.stats;
                         trackStats.experimentStats = trackData.experimentData.stats;
-                        newStatsViewModel.addOrganized(trackStats);
-                        trackId++;
+                        newStat.addStat(trackStats);
                     }
                 }
-                stats.Add(newStatsViewModel);
-            }
-        }
 
-        private void loadAllPlots(LogQuery query)
-        {
+                stats.Add(newStat);
+            }
+            if (stats.Count > 0)
+                selectedStat = stats[0];
+            //plots
             foreach (string variable in query.variables)
             {
-                PlotViewModel newPlotViewModel;
+                PlotViewModel newPlot;
 
                 int lineSeriesId;
                 string plotType = getVariablePlotType(query, variable);
 
                 if (plotType == LoggedVariableViewModel.PlotType.Last.ToString()
                     || plotType == LoggedVariableViewModel.PlotType.Both.ToString())
-                {
+                { 
                     //LAST EVALUATION values: create a new plot for each variable in the result data          
-                    newPlotViewModel = new PlotViewModel(variable, false, true);
+                    newPlot = new PlotViewModel(variable, false, true);
                     //plot data
 
                     for (int i = 0; i < query.resultTracks.Count; i++)
                     {
                         TrackVariableData variableData = query.resultTracks[i].trackData.getVariableData(variable);
-                        //lineSeriesId = newPlotViewModel.addLineSeries(query.resultTracks[i].groupId);
-                        //It's best to show the id than the full groupId name
-                        lineSeriesId = newPlotViewModel.addLineSeries(i.ToString());
+                        lineSeriesId = newPlot.addLineSeries(query.resultTracks[i].groupId);
                         for (int x = 0; x < query.resultTracks[i].trackData.realTime.Length; ++x)
                         {
-                            newPlotViewModel.addLineSeriesValue(lineSeriesId, query.resultTracks[i].trackData.simTime[x]
+                            newPlot.addLineSeriesValue(lineSeriesId, query.resultTracks[i].trackData.simTime[x]
                                 , variableData.lastEpisodeData.values[x]);
                         }
                     }
-                    plots.Add(newPlotViewModel);
+                    plots.Add(newPlot);
                 }
                 if (plotType == LoggedVariableViewModel.PlotType.All.ToString()
                       || plotType == LoggedVariableViewModel.PlotType.Both.ToString())
                 {
                     //AVERAGED EVALUATION values: create a new plot for each variable in the result data
-                    newPlotViewModel = new PlotViewModel(variable + "-episode", false, true);
+                    newPlot = new PlotViewModel(variable + "-episode", false, true);
                     //plot data
                     for (int i = 0; i < query.resultTracks.Count; i++)
                     {
                         TrackVariableData variableData = query.resultTracks[i].trackData.getVariableData(variable);
-                        lineSeriesId = newPlotViewModel.addLineSeries(query.resultTracks[i].groupId);
+                        lineSeriesId = newPlot.addLineSeries(query.resultTracks[i].groupId);
                         for (int x = 0; x < variableData.experimentData.values.Length; ++x)
                         {
-                            newPlotViewModel.addLineSeriesValue(lineSeriesId, x
+                            newPlot.addLineSeriesValue(lineSeriesId, x
                                 , variableData.experimentData.values[x]);
                         }
                     }
-                    plots.Add(newPlotViewModel);
+                    plots.Add(newPlot);
                 }
             }
+            if (plots.Count > 0)
+            {
+                selectedPlot = plots[0];
+                selectedPlot.updateView();
+            }
         }
-
         public void close()
         {
             if (m_parent != null)
