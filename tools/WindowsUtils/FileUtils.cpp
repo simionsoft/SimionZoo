dcb900dfe09438a95e95de466a7ca81c6a5faeb7
borjafdezgauna <borja.fernandez@ehu.eus>
2017-07-21 18:07:42 +0200

    RLSimion: fixed some utility functions that weren't working properly. Now reports can be properly generated again. A test for the C++ utilities has been added.

--- tools/WindowsUtils/FileUtils.cpp
+++ tools/WindowsUtils/FileUtils.cpp
@@ -1,27 +1,43 @@
 #include "FileUtils.h"
+#include <algorithm>
+
+//returns the last position where a bar character is found in s: '/' or '\\'
+//0 if none is found. This is a shortcut because in our context, we can assume that the
+//last bar will never be in the 0th position and avoids using std::string::npos outside
+size_t getLastBarPos(const string& s)
+{
+	size_t lastBar = s.find_last_of('/');
+	size_t lastBar2 = s.find_last_of('\\');
+	if (lastBar == std::string::npos
+		|| (lastBar2 != std::string::npos && lastBar2>lastBar) )
+		lastBar = lastBar2;
+
+	if (lastBar == std::string::npos)
+		return 0;
+	return lastBar;
+}
 
 string getDirectory(const string& filename)
 {
-	size_t last = filename.find_last_of('/', '\\');
-	if (last < 0)
+	size_t lastBar = getLastBarPos(filename);
+	if (lastBar == 0)
 		return "";
-	return filename.substr(0, last + 1);
+	return filename.substr(0, lastBar + 1);
 }
 
-string getFilename(const string& filepath)
+string getFilename(const string& filename)
 {
-	size_t last = filepath.find_last_of('/', '\\');
-	if (last < 0)
+	size_t lastBar = getLastBarPos(filename);
+	if (lastBar == std::string::npos)
 		return "";
-	return filepath.substr(last + 1);
+	return filename.substr(lastBar + 1);
 }
 
 string removeExtension(const string& filename, unsigned int numExtensions)
 {
 	string result = filename;
-	size_t lastExt, lastBar;
-	lastBar = filename.find_last_of('/', '\\');
-	lastExt = result.find_last_of('.');
+	size_t lastExt = filename.find_last_of('.');
+	size_t lastBar = getLastBarPos(filename);
 	int numRemovedExtensions = 0;
 	while (lastExt > 0 && numRemovedExtensions < numExtensions && lastExt>lastBar)
 	{
