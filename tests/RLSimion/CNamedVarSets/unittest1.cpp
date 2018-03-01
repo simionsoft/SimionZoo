0cada31e6a2e1f1cfd9f45ac078d8a5deeef7f7d
borjafdezgauna <borja.fernandez@ehu.eus>
2018-03-01 11:01:16 +0100

    Badger portable installer: added CNTK libraries. Reconfiguring test projects to remove RLSimion-Lib

--- tests/RLSimion/CNamedVarSets/unittest1.cpp
+++ tests/RLSimion/CNamedVarSets/unittest1.cpp
@@ -1,6 +1,6 @@
 #include "stdafx.h"
 #include "CppUnitTest.h"
-#include "../../../RLSimion-Lib/named-var-set.h"
+#include "../../../RLSimion/named-var-set.h"
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
