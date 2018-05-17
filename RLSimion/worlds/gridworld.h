4b9ad95eed8143f77e60a931451e1b975c96c3cb
borjafdezgauna <borja.fernandez@ehu.eus>
2018-05-17 18:14:05 +0200

    Removed the continuous version of the gridworld. Out of the scope of the project

--- RLSimion/worlds/gridworld.h
+++ RLSimion/worlds/gridworld.h
@@ -36,35 +36,3 @@ public:
 	double getMax();
 };
 
-
-//DISCRETE GRIDWORLD
-class DiscreteGridWorld : public DynamicModel
-{
-	size_t m_sXPosition, m_sYPosition;
-
-	INT_PARAM m_targetX, m_targetY;
-
-	//for discrete case
-	size_t m_aStep;
-
-public:
-	DiscreteGridWorld(ConfigNode* pParameters);
-	virtual ~DiscreteGridWorld();
-
-	void reset(State *s);
-
-	void executeAction(State *s, const Action *a, double dt);
-};
-
-class DiscreteGridWorldReward : public IRewardComponent
-{
-protected:
-	size_t m_xTarget, m_yTarget;
-
-public:
-	DiscreteGridWorldReward(int targetX, int targetY);
-	double getReward(const State *s, const Action *a, const State *s_p);
-	const char* getName() { return "reward"; }
-	double getMin();
-	double getMax();
-};
