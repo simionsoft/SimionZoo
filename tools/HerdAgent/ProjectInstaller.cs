73a188c0813bfe5223121dc0f5e6c563efb4bafe
Borja Fernández Gauna <bortxkudai@hotmail.com>
2018-10-17 12:33:10 +0200

    Removed "AnyCPU" platform to simplify compilation

--- tools/HerdAgent/ProjectInstaller.cs
+++ tools/HerdAgent/ProjectInstaller.cs
@@ -76,29 +76,6 @@ namespace Herd
                     sc.Start();
                 }
             }
-
-
-
-            ////add the firewall rules
-            //var proc1 = new ProcessStartInfo();
-            //string command;
-            //proc1.UseShellExecute = true;
-            //proc1.WorkingDirectory = @"c:\windows\system32";
-            //proc1.FileName = @"c:\windows\system32\cmd.exe";
-            //proc1.Verb = "runas";
-            //proc1.WindowStyle = ProcessWindowStyle.Hidden;
-
-            ////tcp inbound rule
-            //command = @"netsh advfirewall firewall add rule name=HerdAgentTCPException protocol=tcp action=allow dir=in service=HerdAgent remoteip=localsubnet localport=" + Herd.HerdAgent.m_comPortHerd;
-            //proc1.Arguments = "/c " + command;
-            //Process.Start(proc1);
-            ////udp inbound rule
-            //command = @"netsh advfirewall firewall add rule name=HerdAgentUDPException protocol=udp action=allow dir=in service=HerdAgent remoteip=localsubnet localport=" + Herd.HerdAgent.m_discoveryPortHerd;
-            //proc1.Arguments = "/c " + command;
-            //Process.Start(proc1);
-
-            ////start the service
-            //new ServiceController(serviceInstaller1.ServiceName).Start();
         }
 
         private void serviceInstaller1_AfterUninstall(object sender, InstallEventArgs e)
@@ -107,24 +84,6 @@ namespace Herd
                 Type.GetTypeFromProgID("HNetCfg.FwPolicy2"));
             firewallPolicy.Rules.Remove(HerdAgent.FirewallExceptionNameTCP);
             firewallPolicy.Rules.Remove(HerdAgent.FirewallExceptionNameUDP);
-            //remove the firewall rules
-            //var proc1 = new ProcessStartInfo();
-            //string command;
-            //proc1.UseShellExecute = true;
-            //proc1.WorkingDirectory = @"c:\windows\system32";
-            //proc1.FileName = @"c:\windows\system32\cmd.exe";
-            //proc1.Verb = "runas";
-            //proc1.WindowStyle = ProcessWindowStyle.Hidden;
-
-            ////tcp inbound rule
-            //command = @"netsh advfirewall firewall delete rule name=HerdAgentTCPException";
-            //proc1.Arguments = "/c " + command;
-            //Process.Start(proc1);
-
-            ////udp inbound rule
-            //command = @"netsh advfirewall firewall delete rule name=HerdAgentUDPException";
-            //proc1.Arguments = "/c " + command;
-            //Process.Start(proc1);
         }
 
         private void serviceProcessInstaller1_AfterInstall(object sender, InstallEventArgs e)
