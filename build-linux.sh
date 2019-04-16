cb6fbc77255b641322c77da8df474fcc906b0855
Borja Fernández Gauna <bortxkudai@hotmail.com>
2019-04-16 17:25:50 +0200

    Reordered geometryLib before freeglut3 in build-linux.sh

--- build-linux.sh
+++ build-linux.sh
@@ -401,6 +401,6 @@ echo ...Finished
 echo Compiling RLSimion-linux...
 mkdir tmp/RLSimion-linux
 g++ -w -x c++ -std=c++11 RLSimion/App/app-main.cpp -o tmp/RLSimion-linux/app-main.o
-g++ -o tmp/RLSimion-linux/RLSimion-linux.exe tmp/RLSimion-linux/*.o -Wl,--no-undefined -Wl,-rpath='${ORIGIN}' "tmp/RLSimion-Lib-linux/RLSimion-Lib-linux.a"  "tmp/OpenGLRenderer-linux/OpenGLRenderer-linux.a" "tmp/freeglut3-linux/freeglut3-linux.a" "tmp/GeometryLib-linux/GeometryLib-linux.a" "tmp/Bullet3-linux/Bullet3-linux.a" "tmp/glew2-linux/glew2-linux.a" "tmp/SOIL-linux/SOIL-linux.a" "tmp/tinyxml2-linux/tinyxml2-linux.a" "tmp/RLSimion-Common-linux/RLSimion-Common-linux.a" "tmp/System-linux/System-linux.a" -l"GL" -l"X11" -l"GLU" -l"dl"
+g++ -o tmp/RLSimion-linux/RLSimion-linux.exe tmp/RLSimion-linux/*.o -Wl,--no-undefined -Wl,-rpath='${ORIGIN}' "tmp/RLSimion-Lib-linux/RLSimion-Lib-linux.a"  "tmp/OpenGLRenderer-linux/OpenGLRenderer-linux.a" "tmp/GeometryLib-linux/GeometryLib-linux.a" "tmp/freeglut3-linux/freeglut3-linux.a" "tmp/Bullet3-linux/Bullet3-linux.a" "tmp/glew2-linux/glew2-linux.a" "tmp/SOIL-linux/SOIL-linux.a" "tmp/tinyxml2-linux/tinyxml2-linux.a" "tmp/RLSimion-Common-linux/RLSimion-Common-linux.a" "tmp/System-linux/System-linux.a" -l"GL" -l"X11" -l"GLU" -l"dl"
 echo ...Finished
 
