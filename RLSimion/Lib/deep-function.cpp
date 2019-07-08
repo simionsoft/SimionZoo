4344d471117b5d8cf52dddfb542d03c3abf1a1b5
Borja Fernández Gauna <borjafdezgauna>
2019-07-08 18:44:12 +0200

    #122 Added part of the NN initialization: input state/action and number of outputs

--- RLSimion/Lib/deep-function.cpp
+++ RLSimion/Lib/deep-function.cpp
@@ -20,19 +20,32 @@ DeepDiscreteQFunction::DeepDiscreteQFunction(ConfigNode* pConfigNode) : DeepFunc
 
 void DeepDiscreteQFunction::deferredLoadStep()
 {
-
+	//add state variables to the list stored by the base-class DeepFunction
+	for (int i = 0; i < m_inputState.size(); i++) m_inputStateVariables.push_back(m_inputState[i]->get());
+	//calculate the total number of discretized actions and set it
+	m_totalNumActionSteps = 1;
+	for (size_t i = 0; i < m_outputAction.size(); i++)
+		m_totalNumActionSteps *= m_numActionSteps.get();
+	
+	//One output for each discretized action
+	m_numOutputs = m_totalNumActionSteps;
 }
 
 DeepContinuousQFunction::DeepContinuousQFunction() {}
 DeepContinuousQFunction::DeepContinuousQFunction(ConfigNode* pConfigNode) : DeepFunction(pConfigNode)
 {
+	//add state/action variables to the lists stored by the base-class DeepFunction
 	m_inputState = MULTI_VALUE_VARIABLE<STATE_VARIABLE>(pConfigNode, "Input-State", "State variables used as input of the Neural Network");
 	m_inputAction = MULTI_VALUE_VARIABLE<ACTION_VARIABLE>(pConfigNode, "Input-Action", "Action variables used as input of the Neural Network");
 }
 
 void DeepContinuousQFunction::deferredLoadStep()
 {
-
+	for (int i = 0; i < m_inputState.size(); i++) m_inputStateVariables.push_back(m_inputState[i]->get());
+	for (int i = 0; i < m_inputAction.size(); i++) m_inputActionVariables.push_back(m_inputState[i]->get());
+	
+	//Only one output: Q(s,a)
+	m_numOutputs = 1;
 }
 
 DeepVFunction::DeepVFunction() {}
@@ -43,17 +56,25 @@ DeepVFunction::DeepVFunction(ConfigNode* pConfigNode) : DeepFunction(pConfigNode
 
 void DeepVFunction::deferredLoadStep()
 {
-
+	//add state variables to the list stored by the base-class DeepFunction
+	for (int i = 0; i < m_inputState.size(); i++) m_inputStateVariables.push_back(m_inputState[i]->get());
+	
+	//Only one output: V(s)
+	m_numOutputs = 1;
 }
 
 DeepDeterministicPolicy::DeepDeterministicPolicy() {}
 DeepDeterministicPolicy::DeepDeterministicPolicy(ConfigNode* pConfigNode) : DeepFunction(pConfigNode)
 {
+	//add state variables to the list stored by the base-class DeepFunction
 	m_inputState = MULTI_VALUE_VARIABLE<STATE_VARIABLE>(pConfigNode, "Input-State", "State variables used as input of the Neural Network");
 	m_outputAction = MULTI_VALUE_VARIABLE<ACTION_VARIABLE>(pConfigNode, "Output-Action", "Action variables used as output of the policy");
 }
 
 void DeepDeterministicPolicy::deferredLoadStep()
 {
-	
+	for (int i = 0; i < m_inputState.size(); i++) m_inputStateVariables.push_back(m_inputState[i]->get());
+
+	//One output per output action: V(s)
+	m_numOutputs = m_outputAction.size();
 }
--- RLSimion/Lib/deep-function.h
+++ RLSimion/Lib/deep-function.h
@@ -4,8 +4,9 @@
 #include "parameters-numeric.h"
 #include "deep-layer.h"
 #include "deferred-load.h"
+#include "../Common/state-action-function.h"
 
-class DeepFunction: public DeferredLoad
+class DeepFunction: public DeferredLoad, StateActionFunction
 {
 	MULTI_VALUE<DeepLayer> m_layers;
 	ENUM_PARAM<DeepLearner> m_learner;
@@ -13,11 +14,21 @@ class DeepFunction: public DeferredLoad
 	INT_PARAM m_minibatchSize;
 	CHILD_OBJECT_FACTORY<NumericValue> m_learningRate;
 protected:
+	int m_numOutputs = 0;
+	vector<string> m_inputStateVariables;
+	vector<string> m_inputActionVariables;
 	DeepFunction() {}
 public:
 	DeepFunction(ConfigNode* pConfigNode);
 
+	//DeferredLoad
 	virtual void deferredLoadStep() = 0;
+
+	//StateActionFunction
+	//virtual unsigned int getNumOutputs() = 0;
+	//virtual vector<double>& evaluate(const State* s, const Action* a) = 0;
+	virtual const vector<string>& getInputStateVariables() { return m_inputStateVariables; }
+	virtual const vector<string>& getInputActionVariables() { return m_inputActionVariables; }
 };
 
 class DeepDiscreteQFunction: public DeepFunction
@@ -25,6 +36,7 @@ class DeepDiscreteQFunction: public DeepFunction
 	MULTI_VALUE_VARIABLE<STATE_VARIABLE> m_inputState;
 	MULTI_VALUE_VARIABLE<ACTION_VARIABLE> m_outputAction;
 	INT_PARAM m_numActionSteps;
+	int m_totalNumActionSteps = 0;
 public:
 	DeepDiscreteQFunction();
 	DeepDiscreteQFunction(ConfigNode* pConfigNode);
