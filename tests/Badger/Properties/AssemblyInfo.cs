ca68fbccf3c8c641b70df42223560bdd6726a3ae
Borja Fernández Gauna <borjafdezgauna>
2020-01-09 18:02:46 +0100

    Updated version to 1.4.3

--- tests/Badger/Properties/AssemblyInfo.cs
+++ tests/Badger/Properties/AssemblyInfo.cs
@@ -16,5 +16,5 @@ using System.Runtime.InteropServices;
 [assembly: Guid("18972f02-dd58-4b32-a48a-abee2eb3b7df")]
 
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.4.2.0")]
-[assembly: AssemblyFileVersion("1.4.2.0")]
+[assembly: AssemblyVersion("1.4.3.0")]
+[assembly: AssemblyFileVersion("1.4.3.0")]
