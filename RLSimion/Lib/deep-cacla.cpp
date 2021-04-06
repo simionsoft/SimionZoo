c7b673aabf495492e41683acdb8cea9f2f0b5dbe
Borja Fernández Gauna <borjafdezgauna>
2021-04-06 14:57:37 +0200

    Integrated unai's chnages to deep cacla

--- RLSimion/Lib/deep-cacla.cpp
+++ RLSimion/Lib/deep-cacla.cpp
@@ -18,7 +18,7 @@ DeepCACLA::DeepCACLA(ConfigNode* pConfigNode)
 {
 	m_actorPolicy = CHILD_OBJECT<DeepDeterministicPolicy>(pConfigNode, "Policy", "Neural Network used to represent the actors policy");
 	m_noiseSignals = MULTI_VALUE_FACTORY<Noise>(pConfigNode, "Exploration-Noise", "Noise signals added to each of the outputs of the deterministic policy");
-	m_criticVFunction= CHILD_OBJECT<DeepVFunction>(pConfigNode, "Value-Function", "Value function learned by the critic");
+	m_criticVFunction = CHILD_OBJECT<DeepVFunction>(pConfigNode, "Value-Function", "Value function learned by the critic");
 	CNTK::WrapperLoader::SetRequirements();
 }
 
@@ -27,7 +27,7 @@ DeepCACLA::~DeepCACLA()
 {
 	if (m_pActorMinibatch) delete m_pActorMinibatch;
 	if (m_pActorNetwork) m_pActorNetwork->destroy();
-	
+
 	if (m_pCriticMinibatch) delete m_pCriticMinibatch;
 	if (m_pCriticOnlineNetwork) m_pCriticOnlineNetwork->destroy();
 	if (m_pCriticTargetNetwork) m_pCriticTargetNetwork->destroy();
@@ -42,50 +42,49 @@ void DeepCACLA::deferredLoadStep()
 
 	//Initialize the actor
 	m_pActorMinibatch = m_actorPolicy->getMinibatch();
-	m_pActorNetwork= m_actorPolicy->getNetworkInstance();
+	m_pActorNetwork = m_actorPolicy->getNetworkInstance();
 	SimionApp::get()->registerStateActionFunction("Policy", m_pActorNetwork);
 
 	//Initialise the critic
 	m_pCriticMinibatch = m_criticVFunction->getMinibatch();
 	m_pCriticOnlineNetwork = m_criticVFunction->getNetworkInstance();
-	m_pCriticTargetNetwork = (IVFunctionNetwork*) m_pCriticOnlineNetwork->clone();
+	m_pCriticTargetNetwork = (IVFunctionNetwork*)m_pCriticOnlineNetwork->clone();
 	SimionApp::get()->registerStateActionFunction("V", m_pCriticOnlineNetwork);
 
 	m_V_s_p = vector<double>(m_pCriticMinibatch->size());
 	m_V_s = vector<double>(m_pCriticMinibatch->size());
 }
 
-double DeepCACLA::update(const State *s, const Action *a, const State *s_p, double r, double probability)
+double DeepCACLA::update(const State* s, const Action* a, const State* s_p, double r, double probability)
 {
 	static int numCriticUpdates = 0;
 	double gamma = SimionApp::get()->pSimGod->getGamma();
 
 	//Actor network
-	if (!m_pActorMinibatch->isFull())
+	//if (!m_pActorMinibatch->isFull())
 	{
 		//add only tuples that produced a positive temporal difference
-		//evaluate V(s)
-		double v_s= m_pCriticTargetNetwork->evaluate(s, a)[0];
+		double v_s = m_pCriticOnlineNetwork->evaluate(s, a)[0];
 
-		//evaluate V(s')
-		double v_s_p= m_pCriticTargetNetwork->evaluate(s_p, a)[0];
+		double v_s_p = m_pCriticTargetNetwork->evaluate(s_p, a)[0];
 
 		double td = r + gamma * v_s_p - v_s;
 
 		if (td > 0)
+		{
 			m_pActorMinibatch->addTuple(s, a, s_p, r);
-	}
-	else
-	{
-		//update the actor: only actions with a positive TD are saved in the minibatch, so we can use them as target
-		m_pActorNetwork->train(m_pActorMinibatch, m_pActorMinibatch->a(), m_actorPolicy->getLearningRate());
-		m_pActorMinibatch->clear();
-	}
+			m_pCriticMinibatch->addTuple(s, a, s_p, r);
+			if (m_pActorMinibatch->isFull())
+			{
+				m_pActorNetwork->train(m_pActorMinibatch, m_pActorMinibatch->a(), m_actorPolicy->getLearningRate());
+				m_pActorMinibatch->clear();
+			}
 
+		}
+	}
 	//Critic network
-	if (!m_pCriticMinibatch->isFull())
-		m_pCriticMinibatch->addTuple(s, a, s_p, r);
-	else
+	m_pCriticMinibatch->addTuple(s, a, s_p, r);
+	if (m_pCriticMinibatch->isFull())
 	{
 		//evaluate V(s)
 		m_pCriticOnlineNetwork->evaluate(m_pCriticMinibatch->s(), m_V_s);
@@ -115,7 +114,7 @@ double DeepCACLA::update(const State *s, const Action *a, const State *s_p, doub
 	return 0.0;
 }
 
-double DeepCACLA::selectAction(const State *s, Action *a)
+double DeepCACLA::selectAction(const State* s, Action* a)
 {
 	vector<double>& policyOutput = m_pActorNetwork->evaluate(s, a);
 	m_actorPolicy->vectorToAction(policyOutput, 0, a);
@@ -129,12 +128,12 @@ double DeepCACLA::selectAction(const State *s, Action *a)
 	size_t noiseSignalIndex;
 	double noise;
 	//if there are less noise signals than output action variables, just use the last one
-	noiseSignalIndex = std::min((size_t) 0, m_noiseSignals.size() - 1);
+	noiseSignalIndex = std::min((size_t)0, m_noiseSignals.size() - 1);
 	noise = m_noiseSignals[noiseSignalIndex]->getSample();
 
 	for (size_t i = 0; i < m_actorPolicy->getUsedActionVariables().size(); i++)
 	{
-		
+
 		double scaleFactor = a->getProperties(m_actorPolicy->getUsedActionVariables()[i].c_str())->getRangeWidth() * 0.5;
 		double scaledNoise = noise * scaleFactor;
 		const char* outputAction = m_actorPolicy->getUsedActionVariables()[i].c_str();
